e982a728a95f569cb2fe455fce6f19fe4117f858
Steven Gutierrez <stevogbmv@gmail.com>
2023-01-04 15:09:52 -0800

    Update JS file and images folder. Created a random number generator to display images from images folder

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const breweryForm = document.getElementById('form')
 const breweryList = document.getElementById('list')
 const brewerySearch = document.getElementById('search')
 let oldDiv = document.getElementById('displayDiv')
-
+let pictures = [
+    "/Images/PouringBeer.jpg",
+    "/Images/drink_on_counter.jpg",
+    "/Images/fish_tacos.jpg",
+    "/Images/full_glass.jpg",
+    "/Images/pouring_mug.jpg",
+    "/Images/tall_glass.jpg"];
 breweryForm.addEventListener('submit', fetchApi)
 
 function fetchApi(event){
@@ -19,12 +25,12 @@ function fetchApi(event){
 };
 
 function createList(breweries){
-    console.log(breweries)
+    // console.log(breweries)
     let i = document.createElement('li')
     i.id = 'newList'
     i.innerHTML = `<p>${breweries.name} </p>`
     breweryList.appendChild(i)
-    console.log(breweries.name)
+    // console.log(breweries.name)
     i.addEventListener('click', ()=>{
         fetch (`https://api.openbrewerydb.org/breweries?by_name=${breweries.name}`,{
             headers: {
@@ -37,14 +43,14 @@ function createList(breweries){
 };
 
 function displayBrewery(breweries){
-    // oldDiv.
-    console.log(breweries)
-    // let j = document.createElement('li')
-    let j =`<img src="/Images/PouringBeer.jpg" alt="Beer being poured" />
+    let randomNum = Math.floor(Math.random() * pictures.length);
+
+    let j =`<img src=${pictures[randomNum]} alt="Beer being poured" />
     <h3>${breweries[0].name}</h3>
     <ul id="Information">
       <li>${breweries[0].brewery_type} Brewery</li>
       <li>${breweries[0].street} ${breweries[0].city}, ${breweries[0].state}</li>
+      <li>Call: (${breweries[0].phone.slice(0,3)})-${breweries[0].phone.slice(3,6)}-${breweries[0].phone.slice(6)}</li>
       <li>
         <a href="${breweries[0].website_url}"
           >Website</a
